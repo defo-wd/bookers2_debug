a32b1303d0c3b50267231bf35136adc62af00f84
EC2 Default User <ec2-user@ip-172-31-27-214.ap-southeast-2.compute.internal>
2023-08-05 12:11:08 +0000

    提出６

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,12 +4,10 @@ class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     current_user.favorite_books << @book
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     current_user.favorite_books.delete(@book)
-    redirect_back(fallback_location: root_path)
   end
 end
