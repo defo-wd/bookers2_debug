dd2f32cc9a93f53bddd8ed43c3802f59f125a12f
EC2 Default User <ec2-user@ip-172-31-27-214.ap-southeast-2.compute.internal>
2023-08-05 11:27:50 +0000

    コメント非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,25 +6,25 @@ class CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     if @comment.save
       flash[:notice] = "コメントを投稿しました。"
-      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = "コメントの投稿に失敗しました。"
-      redirect_back(fallback_location: root_path)
     end
+    redirect_to @book
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_back(fallback_location: root_path, notice: "Comment was successfully destroyed.")
+    flash[:notice] = "コメントを削除しました。"
+    redirect_to @comment.book
   end
-
 
   private
 
   def comment_params
-      params.require(:comment).permit(:content, :book_id)
+    params.require(:comment).permit(:content, :book_id)
   end
+
   def set_book
     @book = Book.find(params[:book_id])
   end
